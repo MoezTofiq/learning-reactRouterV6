0fbc4a11cbfb623cb6b5059e4aa9bc534db7783a
Moeez Tofiq <moeeztofiq@gmail.com>
2025-01-07 22:07:25 +0500

    added loader to the root file

--- react_router_v6/src/main.jsx
+++ react_router_v6/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    loader: rootLoader,
     errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
